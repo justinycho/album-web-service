9a6a9451e3bbd436e72907c7ff66394cd2d9f80b
Justin Cho <justincho99@live.com>
2021-08-05 08:59:55 -0700

    adds film 1

--- index.php
+++ index.php
@@ -73,7 +73,19 @@ function bondJSON(data){
         <a href="box" class="category">Bond Films By International Box Office Totals</a>
         <h3 id="filmtitle">Title Will Go Here</h3>
         <div id="films">
-            <p>Films will go here</p>
+				<div class="film">
+				<b>Film:</b> 1<br />
+				<b>"Title":</b> Dr. No<br />
+				<b>"Year":</b> 1962<br />
+				<b>"Director":</b> Terence Youn,<br />
+				<b>"Producers":</b> Harry Saltzman and Albert R. Broccoli<br />
+				<b>"Writers":</b> Richard Maibaum, Johanna Harwood and Berkely Mather<br />
+				<b>"Composer":</b> Monty Norman<br />
+				<b>"Bond":</b> Sean Connery<br />
+				<b>"Budget":</b> $1,000,000.00<br />
+				<b>"Box Office":</b> $59,567,035.00<br />
+				<div class="pic"><img src="thumbnails/dr-no.jpg"></div>
+			</div>
         </div>
         <div id="output">Results go here</div>
     </body>
