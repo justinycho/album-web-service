bfc35410fcfea4be9ad0e3723e4e72a9e12d1b27
Justin Cho <justincho99@live.com>
2021-08-10 09:30:01 -0700

    finishes assignment

--- index.php
+++ index.php
@@ -2,22 +2,22 @@
 <head>
 <title>Bond Web Service Demo!</title>
 <style>
-	body {
-		font-family:georgia;
-	}
-	.film{
-		border:1px solid #E77DC2;
-		border-radius: 5px;
-		padding: 5px;
-		margin-bottom:5px;
-		position:relative;	
-	}
+    body {
+        font-family:georgia;
+    }
+    .film{
+        border:1px solid #E77DC2;
+        border-radius: 5px;
+        padding: 5px;
+        margin-bottom:5px;
+        position:relative;
+    }
 
-	.pic{
-		position:absolute;
-		right:10px;
-		top:10px;
-	}
+    .pic{
+        position:absolute;
+        right:10px;
+        top:10px;
+    }
 </style>
 <script src="https://code.jquery.com/jquery-latest.js" type="text/javascript"></script>
 
@@ -52,18 +52,50 @@ function toConsole(data)
 function bondJSON(data){
 //JSON processing data goes here
     console.log(data)
+    //this defines the type of info returned
+    $('#filmtitle').html(data.title);
 
+    $('#films').html('');
+
+
+    $.each(data.films,function(i, item){
+        let str = bondTemplate(item);
+
+
+        //$('<div></div>').html(str).appendTo('#films');
+        //$str.appendTo('#films');
+        $('<div></div>').html(str).appendTo('#films');
+    });
 //allows us to see all data on page
-    let myData = JSON.stringify(data,null,4);
+    //let myData = JSON.stringify(data,null,4);
 
-    myData = '<pre>' + myData + '</pre>';
+   // myData = '<pre>' + myData + '</pre>';
 
-    $("#output").html(myData);
+   // $("#output").html(myData);
 
 //this works but the text is all bunched up
 
     //$("#output").text(JSON.stringify(data));
 }
+
+function bondTemplate(film){
+    return `
+        <div class="film">
+            <b>Film:</b> ${film.Film}<br />
+            <b>Title:</b> ${film.Title}<br />
+            <b>Year:</b> ${film.Year}<br />
+            <b>Director:</b> ${film.Director}<br />
+            <b>Producers:</b> ${film.Producers}<br />
+            <b>Writers:</b> ${film.Writers}<br />
+            <b>Composer:</b> ${film.Composer}<br />
+            <b>Bond:</b> ${film.Bond}<br />
+            <b>Budget:</b> ${film.Budget}<br />
+            <b>Box Office:</b> ${film.BoxOffice}<br />
+            <div class="pic"><img src="thumbnails/${film.Image}"></div>
+        </div>
+    `;
+}
+
 
 </script>
 </head>
@@ -73,20 +105,23 @@ function bondJSON(data){
         <a href="box" class="category">Bond Films By International Box Office Totals</a>
         <h3 id="filmtitle">Title Will Go Here</h3>
         <div id="films">
-				<div class="film">
-				<b>Film:</b> 1<br />
-				<b>"Title":</b> Dr. No<br />
-				<b>"Year":</b> 1962<br />
-				<b>"Director":</b> Terence Youn,<br />
-				<b>"Producers":</b> Harry Saltzman and Albert R. Broccoli<br />
-				<b>"Writers":</b> Richard Maibaum, Johanna Harwood and Berkely Mather<br />
-				<b>"Composer":</b> Monty Norman<br />
-				<b>"Bond":</b> Sean Connery<br />
-				<b>"Budget":</b> $1,000,000.00<br />
-				<b>"Box Office":</b> $59,567,035.00<br />
-				<div class="pic"><img src="thumbnails/dr-no.jpg"></div>
-			</div>
+                <!--
+                <div class="film">
+                <b>Film:</b> 1<br />
+                <b>Title:</b> Dr. No<br />
+                <b>Year:</b> 1962<br />
+                <b>Director:</b> Terence Youn,<br />
+                <b>Producers:</b> Harry Saltzman and Albert R. Broccoli<br />
+                <b>Writers:</b> Richard Maibaum, Johanna Harwood and Berkely Mather<br />
+                <b>Composer:</b> Monty Norman<br />
+                <b>Bond:</b> Sean Connery<br />
+                <b>Budget:</b> $1,000,000.00<br />
+                <b>Box Office:</b> $59,567,035.00<br />
+                <div class="pic"><img src="thumbnails/dr-no.jpg"></div>
+                </div>
+                -->
         </div>
         <div id="output">Results go here</div>
     </body>
 </html>
+
