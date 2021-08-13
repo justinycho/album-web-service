37d0ebe2fd52c29c6a8e4385097a2aa81a5f320e
Justin Cho <justincho99@live.com>
2021-08-12 19:22:55 -0700

    testing page

---
+++ data/albums-genre.js
@@ -0,0 +1,46 @@
+{
+	"title": "Albums by Year",
+	"albums":
+	[
+			{
+			"Year":2020,
+			"Title":"Just Cause Y'all Waited",
+			"Artist":"Lil Durk",
+			"Sales":5000000,
+			"Genre":"Hip Hop",
+			"Image":"jcyw2.jpg"
+			},
+			{
+			"Year":2020,
+			"Title":"City On My Back",
+			"Artist":"Rio Da Yung Og",
+			"Sales":3000000,
+			"Genre":"Rap",
+			"Image":"comb.jpg"
+			},
+			{
+			"Year":2020,
+			"Title":"Hardwood Classic",
+			"Artist":"Baby Smoove",
+			"Sales":2000000,
+			"Genre":"Rap",
+			"Image":"hardwood-classic.jpg"
+			},
+			{
+			"Year":2020,
+			"Title":"F*** The World",
+			"Artist":"Brent Faiyaz",
+			"Sales":4000000,
+			"Genre":"R&B",
+			"Image":"ftw.jpg"
+			},
+			{
+			"Year":2021,
+			"Title":25,
+			"Artist":"G Herbo",
+			"Sales":7000000,
+			"Genre":"Rap",
+			"Image":"25.jpg"
+			}
+    ]
+}
--- index.php
+++ index.php
@@ -1,16 +1,15 @@
 <html>
 <head>
-<title>Album Web Service Demo!</title>
+<title>Albums Web Service Demo</title>
 <style>
-    body {
-        font-family:georgia;
-    }
+    body {font-family:georgia;}
+
     .album{
         border:1px solid #E77DC2;
         border-radius: 5px;
         padding: 5px;
         margin-bottom:5px;
-        position:relative;
+        position:relative;    
     }
 
     .pic{
@@ -18,20 +17,36 @@
         right:10px;
         top:10px;
     }
+
 </style>
 <script src="https://code.jquery.com/jquery-latest.js" type="text/javascript"></script>
 
 <script type="text/javascript">
-$(document).ready(function() {
+$(document).ready(function() {  
 
     $('.category').click(function(e){
         e.preventDefault(); //stop default action of the link
         cat = $(this).attr("href");  //get category from URL
         loadAJAX(cat);  //load AJAX and parse JSON file
     });
-});
+});    
+/*
+function loadAJAX(cat)
+{
+    //AJAX connection will go here
+    //alert('cat is: ' + cat);
+
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: "api.php?cat=" + cat,
+        success: bondJSON
+
+    });
 
 
+}
+*/
 function loadAJAX(cat)
 {
    $.ajax({
@@ -47,87 +62,81 @@ function loadAJAX(cat)
    });
 }
 
-
+    
 function toConsole(data)
 {//return data to console for JSON examination
     console.log(data); //to view,use Chrome console, ctrl + shift + j
 }
 
 function bondJSON(data){
-//JSON processing data goes here
-    console.log(data)
-    //this defines the type of info returned
+    //Here is how I see data returned via the console
+    console.log(data);
+
+    //identifies the type of data returned
     $('#filmtitle').html(data.title);
 
+    //clears other clicked films
     $('#films').html('');
 
+    //loop through films and add template
+    
+    $.each(data.albums,function(i,item){
+        let myAlbum = bondTemplate(item);
 
-    $.each(data.albums,function(i, item){
-        let str = bondTemplate(item);
+        $('<div></div>').html(myAlbum).appendTo('#films');
 
-
-        //$('<div></div>').html(str).appendTo('#films');
-        //$str.appendTo('#films');
-        $('<div></div>').html(str).appendTo('#films');
     });
     
-//allows us to see all data on page
-    let myData = JSON.stringify(data,null,4);
-    myData = '<pre>' + myData + '</pre>';
-     $("#output").html(myData);
-
-//this works but the text is all bunched up
-
     //$("#output").text(JSON.stringify(data));
+
+    //this creates a map of the JSON on our page
+    
+    /*
+    let myData = JSON.stringify(data,null,4);
+    myData = "<pre>" + myData + "</pre>"
+    $("#output").html(myData);
+    */
 }
 
 function bondTemplate(album){
+
     return `
         <div class="album">
-            <b>Year:</b> ${album.Year}<br />
-            <b>Title:</b> ${album.Title}<br />
-            <b>Artist:</b> ${album.Artist}<br />
-            <b>Sales:</b> ${album.Sales}<br />
-            <b>Genre:</b> ${album.Genre}<br />
-            <div class="pic"><img src="thumbnails/${album.Image}"></div>
+            <b>Title: </b>${album.Title}</br>
+            <b>Genre: </b>${album.Genre}</br>
+            <b>Company: </b>${album.Company}</br>
+            <b>Year: </b>${album.Year}</br>
+            <b>Rating: </b>${album.Rating}</br>
+            <div class="pic"><img src="thumbnails/${album.Image}" /></div>
         </div>
     `;
+
 }
 
-/*{
-			"Year":2020,
-			"Title":"Just Cause Y'all Waited",
-			"Artist":"Lil Durk",
-			"Sales":5000000,
-			"Genre":"Hip Hop",
-			"Image":"jcyw2.jpg"
-			},
-*/
 </script>
 </head>
     <body>
-    <h1>Album Web Service</h1>
+    <h1>Albums Web Service</h1>
         <a href="year" class="category">Albums By Year</a><br />
-        <a href="genre" class="category">Albums By Genre</a>
+        <a href="title" class="category">Albums By Title</a>
         <h3 id="filmtitle">Title Will Go Here</h3>
         <div id="films">
-                <!--
-                <div class="film">
-                <b>Film:</b> 1<br />
-                <b>Title:</b> Dr. No<br />
-                <b>Year:</b> 1962<br />
-                <b>Director:</b> Terence Youn,<br />
-                <b>Producers:</b> Harry Saltzman and Albert R. Broccoli<br />
-                <b>Writers:</b> Richard Maibaum, Johanna Harwood and Berkely Mather<br />
-                <b>Composer:</b> Monty Norman<br />
-                <b>Bond:</b> Sean Connery<br />
-                <b>Budget:</b> $1,000,000.00<br />
-                <b>Box Office:</b> $59,567,035.00<br />
-                <div class="pic"><img src="thumbnails/dr-no.jpg"></div>
-                </div>
-                -->
+            <!--
+            <div class="film">
+            <b>Film: </b> 1</br>
+            <b>Title: </b> Dr. No</br>
+            <b>Year: </b> 1962</br>
+            <b>Director: </b> Terence Young</br>
+            <b>Producers: </b> Harry Saltzman and Albert R. Broccoli</br>
+            <b>Writers: </b> Richard Maibaum, Johanna Harwood and Berkely Mather</br>
+            <b>Composer: </b> Monty Norman</br>
+            <b>Bond: </b> Sean Connery</br>
+            <b>Budget: </b> $1,000,000.00</br>
+            <b>Box Office:</b> $59,567,035.00</br>
+            <div class="pic"><img src="thumbnails/dr-no.jpg" /></div>
+        </div>
+            -->
         </div>
         <div id="output">Results go here</div>
     </body>
 </html>
-
